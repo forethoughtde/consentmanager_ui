cc955d9400987e187174de092572d4104c4cc7ce
Karthikeyan Palaniswamy <karthikeyan.forethought@gmail.com>
2019-11-04 23:27:15 +0100

    Add link to Logo text so that clicking on the logo will be taken user to home page

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import './Logo.css';
+import Link from '@material-ui/core/Link';
+
 function Logo(){
     return (
         <h2>
-           Consent Manager
+            <Link color="inherit" href="/" className='cm_link' underline="none">
+                Consent Manager
+            </Link>
         </h2>
     );
 }
